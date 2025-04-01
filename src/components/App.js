8ceb06acccb45fe24c77a0c2a93bc57fefdf9f7d
Dev <theabhishek.ofc@gmail.com>
2025-04-01 16:55:03 +0530

    submit

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,16 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [flag, setFlag] = useState(false)
   return (
     <div id="main">
-     
+        <button id="click"
+         onClick={()=> setFlag(true)}
+        > Submit </button>
+
+        {
+          flag == true && <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+        }
     </div>
   );
 }
